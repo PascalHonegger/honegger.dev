1c96489759795c4a92b01911bea6709a5dc3c731
Pascal Honegger <pascal.honegger@bluewin.ch>
2022-08-02 23:39:19 +0200

    feat(setup): bootstrap simple sveltekit app

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Pascal Honegger</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold mt-14 ml-10">Welcome to the glorious website of <span class="underline">Pascal Honegger</span> 👋</h1>
+
+<div class="ml-10">More content is currently in development, follow the latest updates on <a href="https://github.com/PascalHonegger/honegger.dev">GitHub</a></div>
